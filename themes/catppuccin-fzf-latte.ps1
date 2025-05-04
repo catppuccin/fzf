fd3ab96c134abeca7721f16ad251bc2e9defa493
Hammy <58985301+sgoudham@users.noreply.github.com>
2025-05-04 04:22:58 +0100

    refactor: place snippets in separate files (#21)
    * refactor: place snippets in separate files
    
    * docs: move tip

---
+++ themes/catppuccin-fzf-latte.ps1
@@ -0,0 +1,7 @@
+$ENV:FZF_DEFAULT_OPTS=@"
+--color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39
+--color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78
+--color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39
+--color=selected-bg:#BCC0CC
+--color=border:#CCD0DA,label:#4C4F69
+"@
